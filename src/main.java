84434431c83d976dc3d4d5562a3fe140d1cc6423
kucharz96 <kucharz96@wp.pl>
2018-10-16 22:36:55 +0200

    no dobra

--- src/main.java
+++ src/main.java
@@ -26,7 +26,7 @@ import com.googlecode.lanterna.terminal.TerminalResizeListener;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.regex.Pattern;
-
+//SIEEEEEEEEEEEMA
 public class main {
     public static void main(String[] args) {
     //xckcxxzlkclx
